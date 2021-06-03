451d8008625bdbe49dceef051f5db60b47898d62
= <=>
2021-06-03 19:23:39 +0300

    Broken fix

--- lib/shared/views/view_item_view.dart
+++ lib/shared/views/view_item_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/shared/filled_button.dart';
 
 class ViewItemView extends StatefulWidget {
   final String id;
@@ -13,26 +14,35 @@ class ViewItemView extends StatefulWidget {
 class _ViewItemViewState extends State<ViewItemView> {
   @override
   Widget build(BuildContext context) {
-    return 
-    Scaffold(body: ,)
-    Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GestureDetector(
-            child: IntrinsicWidth(
-              child: Image.asset(
-                'images/sofa_demo.jpg',
-                height: 160,
-                width: 1500,
-                fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              child: IntrinsicWidth(
+                child: Image.asset(
+                  'images/sofa_demo.jpg',
+                  height: 160,
+                  width: 1500,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Row(
-            children: [],
-          )
-        ],
+            Row(
+              children: [],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: FilledButton(
+          child: Text('Reserve This Item!'),
+          onPressed: () {
+            print(widget.id + ' was Reserved');
+          },
+        ),
       ),
     );
   }
