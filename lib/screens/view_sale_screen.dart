1e372ad3c29527448ca92b5b392309252388a8b3
= <=>
2021-06-04 04:09:42 +0300

    fix

--- lib/screens/view_sale_screen.dart
+++ lib/screens/view_sale_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/sale.dart';
 import 'package:flutter_app/screens/create_item_screen.dart';
+import 'package:flutter_app/shared/filled_button.dart';
 import 'package:flutter_app/shared/items_list_view.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
@@ -55,13 +56,76 @@ class _ViewSaleScreenState extends State<ViewSaleScreen> {
                     );
                   }).toList();
                 },
-              )
-          ),
+              )),
         ],
       ),
       backgroundColor: const Color(0xfffffbf4),
       body: SafeArea(
-        child: ItemsListView(sale: widget.sale),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: Stack(children: [
+                    Container(
+                      color: Color(0xff333333),
+                      height: double.infinity,
+                      width: double.infinity,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'ALL SALES',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 45,
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Container(
+                                  height: 32,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffFFFBF4),
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(24),
+                                      bottomRight: Radius.circular(24),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
+                ),
+                Container(
+                    child: Expanded(child: ItemsListView(sale: widget.sale))),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: FilledButton.text(
+                  'NEW SALE',
+                  width: double.infinity,
+                  onPressed: () {
+                    print('clicked');
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
